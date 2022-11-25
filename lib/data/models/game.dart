8d7d5ad37e79478eac6fcedebbab013d4023146c
AlexTHELEGOFAN <alexandre.pozzi69@gmail.com>
2022-11-25 08:49:01 +0100

    initial commit

--- lib/data/models/game.dart
+++ lib/data/models/game.dart
@@ -1,32 +1,32 @@
-import 'dart:ffi';
-
 class Game {
   int id;
-  String name;
+  String name, icon;
   double price;
 
   Game({
     required this.id,
     required this.name,
     required this.price,
+    required this.icon,
   });
 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': this.id,
-      'name': this.name,
-      'price': this.price,
-    };
-  }
+  // Map<String, dynamic> toMap() {
+  //   return {
+  //     'id': id,
+  //     'name': name,
+  //     'price': price,
+  //     'icon': icon,
+  //   };
+  // }
 
-  static dynamic getListMap(List<dynamic> items) {
-    if (items == null) {
-      return null;
-    }
-    List<Map<String, dynamic>> list = [];
-    items.forEach((element) {
-      list.add(element.toMap());
-    });
-    return list;
-  }
+  // static dynamic getListMap(List<dynamic> items) {
+  //   if (items == null) {
+  //     return null;
+  //   }
+  //   List<Map<String, dynamic>> list = [];
+  //   items.forEach((element) {
+  //     list.add(element.toMap());
+  //   });
+  //   return list;
+  // }
 }
--- lib/data/repositories/game_repository.dart
+++ lib/data/repositories/game_repository.dart
@@ -1,20 +1,17 @@
-import 'package:brickdata/data/models/game.dart';
-import 'package:dio/dio.dart';
 import 'package:game_shop/data/models/game.dart';
 
 class GameRepository {
   GameRepository();
 
   List<Game> games = [
-    Game(id: 1, name: "Game1", price: 12.99),
-    Game(id: 2, name: "Game2", price: 12.99),
-    Game(id: 3, name: "Game2", price: 12.99)
+    Game(id: 1, name: "Game1", price: 12.99, icon: "icon"),
+    Game(id: 2, name: "Game2", price: 12.99, icon: "icon"),
+    Game(id: 3, name: "Game2", price: 12.99, icon: "icon")
   ];
 
-  Future<List<Game>> getAllGame() {
-    var gamesMap = games.map((e) {
-      return {"name": e.name, "price": e.price};
-    }).toList();
-    print(gamesMap);
-  }
+  var gamesMap = games.map((e) {
+    return {e.id, e.name, e.price, e.icon};
+  }).toList();
+
+  print(gamesMap);
 }
